50501c3cf717fdac9052a921dc9713d3bb794303
krushi24112002 <krushimonpara24@gmail.com>
2023-01-02 13:16:04 -0500

    JAVA AND OOPS

---
+++ OOPS IN JAVA/Abstraction/OOPsAbstraction3.java
@@ -0,0 +1,31 @@
+// Java Program to illustrate Abstract class Without any abstract method.
+
+// Class 1
+// Abstract Class
+abstract class Base {
+
+    // Demo method. This is not an abstract method.
+    void fun() {
+        System.out.println("Function of Base class is called");
+    }
+}
+
+// Class 2
+class Derived extends Base {
+
+    // This class only inherits the Base class methods and properties.
+
+}
+
+// Main Class
+public class OOPsAbstraction3 {
+
+    public static void main(String[] args) {
+
+        // Creating object of class 2
+        Derived d = new Derived();
+
+        // Calling function defined in class 1 inside main() with object of class 2 inside main() method
+        d.fun();
+    }
+}
