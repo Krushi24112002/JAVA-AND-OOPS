6682473ecae20ac91850210bc3607f2f9fa24ebc
krushi24112002 <krushimonpara24@gmail.com>
2023-01-18 15:44:12 -0500

    JAVA AND OOPS

---
+++ JAVA/Exceptions/BuiltinException5.java
@@ -0,0 +1,14 @@
+// ArrayIndexOutOfBounds Exception
+
+public class BuiltinException5 {
+
+    public static void main(String[] args) {
+        
+        try{
+            int[] arr = new int[4];
+            arr[5]= 24;
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.print("Array Index is Out Of Bounds");
+        }
+    }
+}
