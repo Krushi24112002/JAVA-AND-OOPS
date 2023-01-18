8677c74616ef12e4adb4bb33f3a2734608c5e731
krushi24112002 <krushimonpara24@gmail.com>
2023-01-18 15:36:06 -0500

    JAVA AND OOPS

---
+++ JAVA/Exceptions/BuiltinException3.java
@@ -0,0 +1,15 @@
+// StringIndexOutOfBound Exception
+
+public class BuiltinException3 {
+
+    public static void main(String[] args) {
+
+        try {
+            String str = "Hello Krushi"; // length is 11
+            char c = str.charAt(12);
+            System.out.print(c);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.print("StringIndexOutOfBoundsException");
+        }
+    }
+}
