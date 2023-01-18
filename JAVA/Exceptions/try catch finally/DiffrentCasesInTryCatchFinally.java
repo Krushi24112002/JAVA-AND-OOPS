632095a57490a6977207f5671c6848987ca514ab
krushi24112002 <krushimonpara24@gmail.com>
2023-01-18 16:18:14 -0500

    JAVA AND OOPS

---
+++ JAVA/Exceptions/try catch finally/DiffrentCasesInTryCatchFinally.java
@@ -0,0 +1,12 @@
+/*
+
+1.Control flow in try-catch clause OR try-catch-finally clause 
+   Case 1: Exception occurs in try block and handled in catch block
+   Case 2: Exception occurs in try-block is not handled in catch block
+   Case 3: Exception doesn’t occur in try-block
+
+2.try-finally clause 
+   Case 1: Exception occurs in try block
+   Case 2: Exception doesn’t occur in try-block
+   
+ */
