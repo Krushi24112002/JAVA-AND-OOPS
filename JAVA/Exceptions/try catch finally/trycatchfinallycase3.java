3994d98ecb8af6a81132f51f25a1ce6b01031a55
krushi24112002 <krushimonpara24@gmail.com>
2023-01-18 16:44:10 -0500

    JAVA AND OOPS

---
+++ JAVA/Exceptions/try catch finally/trycatchfinallycase3.java
@@ -0,0 +1,19 @@
+// Exception doesn’t occur in try-block (Not Use Finally Block)
+
+public class trycatchfinallycase3 {
+
+    public static void main(String[] args) {
+        
+        try{
+
+            String str = "2411";
+            int num = Integer.parseInt(str);
+            System.out.println("Inside try block");
+        }catch(NumberFormatException e){
+            System.out.println("catch block executed...");
+        }
+
+        // rest program will not execute
+        System.out.println("Outside try-catch clause");
+    }
+}
---
+++ JAVA/Exceptions/try catch finally/trycatchfinallycase3a.java
@@ -0,0 +1,21 @@
+// Exception doesn’t occur in try-block (Use Finally Block)
+
+public class trycatchfinallycase3a {
+
+    public static void main(String[] args) {
+
+        try {
+
+            String str = "2411";
+            int num = Integer.parseInt(str);
+            System.out.println("Inside try block");
+        } catch (NumberFormatException e) {
+            System.out.println("catch block executed...");
+        } finally {
+            System.out.println("finally block executed");
+        }
+
+        // rest program will not execute
+        System.out.println("Outside try-catch clause");
+    }
+}
