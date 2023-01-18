1524b0dfe8a16d22b9781a115f4470b90f0eb57a
krushi24112002 <krushimonpara24@gmail.com>
2023-01-18 15:06:28 -0500

    JAVA AND OOPS

---
+++ JAVA/Exceptions/Method2ToPrintExceptionInformation.java
@@ -0,0 +1,22 @@
+// toString()
+/*
+
+toString() Mrthod :- This method prints exception information in the format of Name of the exception: description of the exception.
+
+ */
+
+// program to print the exception information using toString() method
+
+public class Method2ToPrintExceptionInformation {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+        try {
+            System.out.print(a / b);
+        } catch (ArithmeticException e) {
+            System.out.print(e.toString());
+        }
+    }
+}
