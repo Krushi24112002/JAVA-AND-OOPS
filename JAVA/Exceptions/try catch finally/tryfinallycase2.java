0761a8c533f16b6b9e7d7a613afc23d007f1fac2
krushi24112002 <krushimonpara24@gmail.com>
2023-01-18 16:52:21 -0500

    JAVA AND OOPS

---
+++ JAVA/Exceptions/try catch finally/tryfinallycase2.java
@@ -0,0 +1,18 @@
+// Exception not raised :- If an exception does not occur in try block then control flow will be finally block followed by rest of the program
+
+public class tryfinallycase2 {
+
+    public static void main(String[] args) {
+
+        try {
+            String str = "2411";
+            int num = Integer.parseInt(str);
+            System.out.println("Inside try block");
+        } finally {
+            System.out.println("finally block executed");
+        }
+
+        // rest program will be executed
+        System.out.println("Outside try-finally clause");
+    }
+}
