74ad618cf5143083bac2d37d82f54d2991fad9ab
krushi24112002 <krushimonpara24@gmail.com>
2023-01-18 16:05:22 -0500

    JAVA AND OOPS

---
+++ JAVA/Exceptions/trycatchfinallyworking.java
@@ -0,0 +1,16 @@
+public class trycatchfinallyworking {
+
+    public static void main(String[] args) {
+
+        int a = 24;
+        int b = 0;
+
+        try {
+            System.out.print(" Result " + a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Exception caught:Division by zero");
+        } finally {
+            System.out.println("I am in final block");
+        }
+    }
+}
