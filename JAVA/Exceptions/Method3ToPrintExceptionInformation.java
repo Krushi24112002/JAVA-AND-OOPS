522216eafbfd09adf5f6505510a2f35b4eb2a265
krushi24112002 <krushimonpara24@gmail.com>
2023-01-18 15:10:47 -0500

    JAVA AND OOPS

---
+++ JAVA/Exceptions/Method3ToPrintExceptionInformation.java
@@ -0,0 +1,23 @@
+// getMessage()
+
+/*
+
+getMessage() Method :- This method prints only the description of the exception.
+
+ */
+
+// program to print the exception information using getMessage() method
+
+public class Method3ToPrintExceptionInformation {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+        try {
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.print(e.getMessage());
+        }
+    }
+}
