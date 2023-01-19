9ca4ffb0c73f76147b8c43c6918caf07a8e960a2
krushi24112002 <krushimonpara24@gmail.com>
2023-01-19 16:30:06 -0500

    JAVA AND OOPS AND COLLECTION FRAMEWORKS

---
+++ JAVA COLLECTION FRAMEWORKS/DEQUE INTERFACE/DequeBasicInformations.java
@@ -0,0 +1,9 @@
+// Deque Interface
+/*
+
+Deque Interface :-
+-> he Deque is related to the double-ended queue that supports the addition or removal of elements from either end of the data structure.
+It can either be used as a queue(first-in-first-out/FIFO) or as a stack(last-in-first-out/LIFO). Deque is the acronym for double-ended queue.
+
+
+ */
