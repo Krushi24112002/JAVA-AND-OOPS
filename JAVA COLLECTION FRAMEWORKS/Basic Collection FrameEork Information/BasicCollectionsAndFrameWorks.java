465897f6149c61217837f160642de52660004c64
krushi24112002 <krushimonpara24@gmail.com>
2023-01-19 03:26:42 -0500

    JAVA AND OOPS AND COLLECTION FRAMEWORKS

--- JAVA COLLECTION FRAMEWORKS/Basic Collection FrameEork Information/BasicCollectionsAndFrameWorks.java
+++
@@ -1,3 +0,0 @@
-// Collections :- Any group of individual objects which are represented as a single unit is known as the collection of the objects.
-
-// FrameWorks :- A framework is a set of classes and interfaces which provide a ready-made architecture. 
