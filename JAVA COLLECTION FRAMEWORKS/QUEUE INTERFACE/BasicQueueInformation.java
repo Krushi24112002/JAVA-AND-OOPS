63d7ac8cacbc62b154651aa16d36a4cdb8d9c840
krushi24112002 <krushimonpara24@gmail.com>
2023-01-19 13:53:09 -0500

    JAVA AND OOPS AND COLLECTION FRAMEWORKS

---
+++ JAVA COLLECTION FRAMEWORKS/QUEUE INTERFACE/BasicQueueInformation.java
@@ -0,0 +1,13 @@
+// Queue
+/*
+
+Queue :-The Queue is used to insert elements at the end of the queue and removes from the beginning of the queue.
+It follows FIFO concept.
+
+# Operations on Queue Interface # 
+
+1. Adding Elements :- Using offer() Method
+2. Removing Elements :- Using poll() Method
+3. Accessing the Element :- Using peek() Method
+
+ */
---
+++ JAVA COLLECTION FRAMEWORKS/QUEUE INTERFACE/QueueOperation.java
@@ -0,0 +1,35 @@
+// Queue
+
+import java.util.*;
+
+public class QueueOperation {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> val = new LinkedList<>();
+
+        // 1/Adding Elements :- Using offer() Method
+        val.offer(10);
+        val.offer(20);
+        val.offer(30);
+        val.offer(40);
+
+        System.out.println("Print Values = " + val);
+
+        // Iterating over Queue
+        // Method :- 1 Using For each Loop
+        for (Integer elements : val) {
+            System.out.println(elements + " ");
+        }
+
+        // 2.Removing Elements :- Using poll() Method
+        System.out.println("Which Element is poll = " + val.poll());
+
+        // One Elemtent Poll After Queue Elements Is
+        System.out.println("One Elemtent Poll After Queue Elements Is = " + val);
+
+        // 3.Accessing the Element :- Using peek() Method
+        System.out.println("Find Next Peek Elemets = " + val.peek());
+
+    }
+}
