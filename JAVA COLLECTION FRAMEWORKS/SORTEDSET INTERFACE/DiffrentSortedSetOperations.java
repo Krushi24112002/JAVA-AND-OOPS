bfe0c4b796faf0e483438eb7848b2aeaec128ee5
krushi24112002 <krushimonpara24@gmail.com>
2023-01-19 21:52:25 -0500

    JAVA AND OOPS AND COLLECTION FRAMEWORKS

---
+++ JAVA COLLECTION FRAMEWORKS/SORTEDSET INTERFACE/DiffrentSortedSetOperations.java
@@ -0,0 +1,40 @@
+// SortedSet
+// #NOTE# :- This Set Mian Use in Sort The Elements
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class DiffrentSortedSetOperations {
+
+    public static void main(String[] args) {
+
+        SortedSet<String> s1 = new TreeSet<>();
+
+        s1.add("India");
+        s1.add("Australia");
+        s1.add("South Afric");
+
+        // Print Elements in Sorted Order.
+        System.out.println("Print Elements in Sorted Order " + s1);
+
+        SortedSet<Character> s2 = new TreeSet<>();
+
+        s2.add('B');
+        s2.add('A');
+        s2.add('C');
+        s2.add('A');
+
+        System.out.println("Print Elements in Sorted Order And Duplicate Elements Print Only One Time " + s2);
+
+        SortedSet<Integer> s3 = new TreeSet<>();
+
+        s3.add(12);
+        s3.add(9);
+        s3.add(18);
+        s3.add(1);
+        s3.add(9);
+
+        System.out.println("Print Elements in Sorted Order And Duplicate Elements Print Only One Time " + s3);
+
+    }
+}
