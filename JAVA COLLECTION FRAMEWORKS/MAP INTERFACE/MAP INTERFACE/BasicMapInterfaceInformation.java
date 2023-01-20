5a884da8cb88aa192db2c5109b3e410f61ba0862
krushi24112002 <krushimonpara24@gmail.com>
2023-01-20 09:10:10 -0500

    JAVA AND OOPS AND COLLECTION FRAMEWORKS

---
+++ JAVA COLLECTION FRAMEWORKS/MAP INTERFACE/MAP INTERFACE/BasicMapInterfaceInformation.java
@@ -0,0 +1,4 @@
+// Map Interface
+// #NOTE# :- Map Is Different Compare to Other Java Collections (List,Set,Queue)
+// #NOTE# :- In Map Interface Key And Value Pair Are Available
+// #NOTE# :- A map contains unique keys.
---
+++ JAVA COLLECTION FRAMEWORKS/MAP INTERFACE/MAP INTERFACE/DifferentMapOperations.java
@@ -0,0 +1,61 @@
+// Map
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class DifferentMapOperations {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> s1 = new HashMap<>();
+
+        // put() :- This method is used to associate the specified value with the
+        // specified key in this map.
+        s1.put("One", 1);
+        s1.put("Three", 3);
+        s1.put("Two", 2);
+        s1.put("Five", 5);
+        s1.put("Four", 4);
+
+        // Print Key = Value Pairs
+        System.out.println("Key : Values " + "= " + s1);
+
+        // Iterating the Map
+        // Method 1 Using for each loop
+        for (Map.Entry<String, Integer> e : s1.entrySet()) {
+            // Print All Entry (key:Value)
+            System.out.println("Print All Entrys With Key : Value " + e);
+
+        }
+
+        // KeySet() :- Only Print List Of All Keys
+        System.out.println("KeySet() " + s1.keySet());
+
+        // values() :- Only Print List Of All Values.
+        System.out.println("values() " + s1.values());
+
+        // isEmpty() :- This method is used to check if a map is having any entry for
+        // key and value pairs. If no mapping exists, then this returns true.
+        System.out.println("isEmpty() " + s1.isEmpty());
+
+        // size() :- This method is used to return the number of key/value pairs
+        // available in the map.
+        System.out.println("size of Key:value Pairs = " + s1.size());
+
+        // containsKey() :- This method is used to check whether a particular key is
+        // being mapped into the Map or not. It takes the key element as a parameter and
+        // returns True if that element is mapped in the map.
+        System.out.println("containsKey() " + s1.containsKey("Five"));
+
+        // containsValue() :- This method is used to check whether a particular value is
+        // being mapped by a single or more than one key in the Map. It takes the value
+        // as a parameter and returns True if that value is mapped by any of the key in
+        // the map.
+        System.out.println("containsValue() " + s1.containsValue(4));
+
+        // hashCode() :- This method is used to generate a hashCode for the given map
+        // containing keys and values.
+        System.out.println("Key:Value Pair hashCode Is = " + s1.hashCode());
+
+    }
+}
