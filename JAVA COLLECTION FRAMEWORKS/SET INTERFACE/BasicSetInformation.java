8ba0a78ac6cefaf7fb1fe45fa754a741fdee886b
krushi24112002 <krushimonpara24@gmail.com>
2023-01-19 19:42:31 -0500

    JAVA AND OOPS AND COLLECTION FRAMEWORKS

--- JAVA COLLECTION FRAMEWORKS/SET INTERFACE/BasicSetInformation.java
+++ JAVA COLLECTION FRAMEWORKS/SET INTERFACE/BasicSetInformation.java
@@ -4,4 +4,7 @@
 -> It is an unordered collection of objects in which duplicate values cannot be stored.
 -> in Set Pass to Values 1.key , 2.Value
 
+Set-> Interface
+Set Classes -> HashSet, LinkedHashSet ,Treeset
+
  */
