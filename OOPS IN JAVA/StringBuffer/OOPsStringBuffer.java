e8e087e2240c6a6d587da982740184665848c594
KRUSHI MONPARA <krushimonpara24@gmail.com>
2023-08-07 11:41:38 +0530

    JAVA AND OOPS

--- OOPS IN JAVA/StringBuffer/OOPsStringBuffer.java
+++ OOPS IN JAVA/StringBuffer/OOPsStringBuffer.java
@@ -14,57 +14,59 @@
 -> The advantage of StringBuilder is faster performance.
 However, in cases in which you are using multithreading,  you must use StringBuffer rather than StringBuilder.
 
+#NOTE# 
+1) StringBuffer is synchronized i.e. thread safe. It means two threads can't call the methods of StringBuffer simultaneously.
+2) StringBuffer is less efficient than StringBuilder.
+
  */
 
 public class OOPsstringbuffer {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    // 1.Append Method In StringBuffer
+    // NOTE :- Used to add text at the end of the existing text.
+    StringBuffer sb = new StringBuffer("Hello ");
+    sb.append("Joy"); // now original string is changed
+    System.out.println(sb);
 
-        // 1.Append Method In StringBuffer
-        // NOTE :- Used to add text at the end of the existing text.
-        StringBuffer sb = new StringBuffer("Hello ");
-        sb.append("Joy"); // now original string is changed
-        System.out.println(sb);
+    // 2.insert Method In StringBuffer
+    // NOTE :- Inserts text at the specified index position
+    StringBuffer sb1 = new StringBuffer("You Can ");
+    sb1.insert(8, "Achive");
+    System.out.println(sb1);
 
-        // 2.insert Method In StringBuffer
-        // NOTE :- Inserts text at the specified index position
-        StringBuffer sb1 = new StringBuffer("You Can ");
-        sb1.insert(8, "Achive");
-        System.out.println(sb1);
+    // object
+    StringBuffer sb2 = new StringBuffer("IJAVA");
+    sb2.replace(1, 1, "LOVE");
+    System.out.println(sb2);
 
-        // object
-        StringBuffer sb2 = new StringBuffer("IJAVA");
-        sb2.replace(1, 1, "LOVE");
-        System.out.println(sb2);
+    // 4. delete Method In StringBuffer
+    // NOTE :- Deletes a sequence of characters from the invoking object
+    StringBuffer sb3 = new StringBuffer("Hello");
+    sb3.delete(1, 3);
+    System.out.println(sb3);
 
-        // 4. delete Method In StringBuffer
-        // NOTE :- Deletes a sequence of characters from the invoking object
-        StringBuffer sb3 = new StringBuffer("Hello");
-        sb3.delete(1, 3);
-        System.out.println(sb3);
+    // 5. deleteCharAt Method In StringBuffer
+    // NOTE :- Deletes the character at the index specified by location
+    StringBuffer sb5 = new StringBuffer("Jhony");
+    sb5.deleteCharAt(4);
+    System.out.println(sb5);
 
-        // 5. deleteCharAt Method In StringBuffer
-        // NOTE :- Deletes the character at the index specified by location
-        StringBuffer sb5 = new StringBuffer("Jhony");
-        sb5.deleteCharAt(4);
-        System.out.println(sb5);
+    // 6. reverse Method In StringBuffer
+    // Reverse the characters within a StringBuffe
+    StringBuffer sb6 = new StringBuffer("KRUSHI");
+    sb6.reverse();
+    System.out.println(sb6);
 
-        // 6. reverse Method In StringBuffer
-        // Reverse the characters within a StringBuffe
-        StringBuffer sb6 = new StringBuffer("KRUSHI");
-        sb6.reverse();
-        System.out.println(sb6);
+    // 7. Length Method In StringBuffer
+    // NOTE :- Returns length of the string
+    StringBuffer sb7 = new StringBuffer("KRUSHI");
+    System.out.println("Your String Length Is " + sb7.length());
 
-        // 7. Length Method In StringBuffer
-        // NOTE :- Returns length of the string
-        StringBuffer sb7 = new StringBuffer("KRUSHI");
-        System.out.println("Your String Length Is " + sb7.length());
-
-        // 8.charAt Method In StringBuffer
-        // NOTE :- This method returns the char value in this sequence at the specified
-        // index.
-        StringBuffer sb8 = new StringBuffer("KRUSHI");
-        System.out.println(sb8.charAt(3));
-
-    }
+    // 8.charAt Method In StringBuffer
+    // NOTE :- This method returns the char value in this sequence at the specified
+    // index.
+    StringBuffer sb8 = new StringBuffer("KRUSHI");
+    System.out.println(sb8.charAt(3));
+  }
 }
