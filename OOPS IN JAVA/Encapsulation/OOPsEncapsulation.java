c031af98423f23935fd317df55344f51a5b32b8c
KRUSHI MONPARA <krushimonpara24@gmail.com>
2023-08-07 15:09:20 +0530

    JAVA AND OOPS

--- OOPS IN JAVA/Encapsulation/OOPsEncapsulation.java
+++ OOPS IN JAVA/Encapsulation/OOPsEncapsulation.java
@@ -1,6 +1,10 @@
 // Encapsulation
 // Definition : Wrapping Up Data Member And Function(Method)
-// Data Member -> Properties OR State 
+/*
+Encapsulation can be achieved by Declaring all the variables in the class as private and writing public methods in the class to set and get
+the values of variables. 
+ */
+// Data Member -> Properties OR State
 // Function -> Method OR Behavior
 // NOTE: Fully Capsulated Class -> All Data Member(Properties) Are Private. Only Same Class Can Access This Data Mamber(Properties).
 
@@ -13,52 +17,49 @@
 // Creating Class
 class Encapsulation {
 
-    // Creating Class Properties
-    private String name;
-    private String surname;
-    private int age;
+  // Creating Class Properties
+  private String name;
+  private String surname;
+  private int age;
 
-    String getname() {
-        return name;
-        // OR This Type Can Also Write
-        // return this.name;
-    }
+  String getname() {
+    return name;
+    // OR This Type Can Also Write
+    // return this.name;
+  }
 
-    String getsurname() {
-        return surname;
-    }
+  String getsurname() {
+    return surname;
+  }
 
-    int getage() {
-        return age;
-    }
+  int getage() {
+    return age;
+  }
 
-    void setname(String PersonName) {
-        name = PersonName;
-    }
+  void setname(String PersonName) {
+    name = PersonName;
+  }
 
-    void setsurname(String PersonSurName) {
-        surname = PersonSurName;
-    }
+  void setsurname(String PersonSurName) {
+    surname = PersonSurName;
+  }
 
-    void setage(int PersonAge) {
-        age = PersonAge;
-    }
-
+  void setage(int PersonAge) {
+    age = PersonAge;
+  }
 }
 
 public class OOPsEncapsulation {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    // Creating Class Object Using new KeyWord
+    Encapsulation PersonDetail = new Encapsulation();
+    PersonDetail.setname("KRUSHI");
+    PersonDetail.setsurname("MONPARA");
+    PersonDetail.setage(19);
 
-        // Creating Class Object Using new KeyWord
-        Encapsulation PersonDetail = new Encapsulation();
-        PersonDetail.setname("KRUSHI");
-        PersonDetail.setsurname("MONPARA");
-        PersonDetail.setage(19);
-
-        System.out.println("Your Name Is " + PersonDetail.getname());
-        System.out.println("Your SurName Is " + PersonDetail.getsurname());
-        System.out.println("Your Age Is " + PersonDetail.getage());
-
-    }
+    System.out.println("Your Name Is " + PersonDetail.getname());
+    System.out.println("Your SurName Is " + PersonDetail.getsurname());
+    System.out.println("Your Age Is " + PersonDetail.getage());
+  }
 }
