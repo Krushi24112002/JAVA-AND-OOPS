f0bb22aee96fdc6ef98c0e214ea2942877a3e5ac
KRUSHI MONPARA <krushimonpara24@gmail.com>
2023-08-07 19:34:45 +0530

    JAVA AND OOPS

--- OOPS IN JAVA/KeyWord/Super Keyword/OOPsInvokeTheImmediateParentClassConstructorUsingSuperKeyWord.java
+++ OOPS IN JAVA/KeyWord/Super Keyword/OOPsInvokeTheImmediateParentClassConstructorUsingSuperKeyWord.java
@@ -3,30 +3,23 @@
 // Creating Parent Class
 class Animal {
 
-    // Creating Parent Class Constructor
-    Animal() {
-        System.out.println("Animal Is Very Important For World");
-    }
+  // Creating Parent Class Constructor
+  Animal() {
+    System.out.println("Animal Is Very Important For World");
+  }
 }
 
 // Creating Sub Class
 class Dog extends Animal {
 
-    // Creating Sub Class Constructor
-    Dog() {
-
-        super();// Calling Parent Class Constructor
-        System.out.println("Dog Is Very Funny Animal");
-    }
+  // Creating Sub Class Constructor
+  Dog() {
+    super(); // Calling Parent Class Constructor
+    System.out.println("Dog Is Very Funny Animal");
+  }
 }
 
 public class OOPsInvokeTheImmediateParentClassConstructorUsingSuperKeyWord {
 
-    public static void main(String[] args) {
-
-        // Crating Object Using new KeyWord
-        Dog d = new Dog();
-
-    }
-
+  public static void main(String[] args) {}
 }
