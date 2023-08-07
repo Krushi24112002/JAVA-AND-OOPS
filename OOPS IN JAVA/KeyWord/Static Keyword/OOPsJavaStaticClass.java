3e6e6a36050e1302072ede54a7692abcc9d070b7
KRUSHI MONPARA <krushimonpara24@gmail.com>
2023-08-07 19:31:31 +0530

    JAVA AND OOPS

--- OOPS IN JAVA/KeyWord/Static Keyword/OOPsJavaStaticClass.java
+++ OOPS IN JAVA/KeyWord/Static Keyword/OOPsJavaStaticClass.java
@@ -1,24 +1,21 @@
-
 public class OOPsJavaStaticClass {
 
-    // Creating static nested Class
-    static class MyNestedClass {
+  // Creating static nested Class
+  static class MyNestedClass {
 
-        String Name;
-        int Age;
+    String Name;
+    int Age;
 
-        void Detail() {
-            System.out.println("Your Name is " + Name);
-            System.out.println("Your Age Is " + Age);
-        }
+    void Detail() {
+      System.out.println("Your Name is " + Name);
+      System.out.println("Your Age Is " + Age);
     }
+  }
 
-    public static void main(String[] args) {
-
-        OOPsJavaStaticClass.MyNestedClass obj = new OOPsJavaStaticClass.MyNestedClass();
-        obj.Name = "KRUSHI MONPARA";
-        obj.Age = 20;
-        obj.Detail();
-
-    }
+  public static void main(String[] args) {
+    OOPsJavaStaticClass.MyNestedClass obj = new OOPsJavaStaticClass.MyNestedClass();
+    obj.Name = "KRUSHI MONPARA";
+    obj.Age = 20;
+    obj.Detail();
+  }
 }
--- OOPS IN JAVA/KeyWord/Static Keyword/OOPsJavaStaticVariable.java
+++ OOPS IN JAVA/KeyWord/Static Keyword/OOPsJavaStaticVariable.java
@@ -10,43 +10,43 @@
 // Creating Class
 class Student {
 
-    // Creating Class Properties
-    String name;
-    int RollNumber;
-    // Creating Static Variable
-    static String College = "IIT";
+  // Creating Class Properties
+  String name;
+  int RollNumber;
+  // Creating Static Variable
+  static String College = "IIT";
 
-    // Craeting Parameterized Constructor
-    Student(String StudentName, int StudentRollNumber) {
-        name = StudentName;
-        RollNumber = StudentRollNumber;
-    }
+  // Craeting Parameterized Constructor
+  Student(String StudentName, int StudentRollNumber) {
+    name = StudentName;
+    RollNumber = StudentRollNumber;
+  }
 
-    // Creating Print Class Function
-    void StudentDetail() {
-        System.out.println(name + " " + RollNumber + " " + College);
-    }
-
+  // Creating Print Class Function
+  void StudentDetail() {
+    System.out.println(name + " " + RollNumber + " " + College);
+  }
 }
 
 public class OOPsJavaStaticVariable {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    // Creating Object
+    Student s1 = new Student("KRUSHI", 24);
+    Student s2 = new Student("ASHRUTA", 21);
 
-        // Creating Object
-        Student s1 = new Student("KRUSHI", 24);
-        Student s2 = new Student("ASHRUTA", 21);
+    s1.StudentDetail();
+    s2.StudentDetail();
 
-        s1.StudentDetail();
-        s2.StudentDetail();
+    s1.College = "MIT";
 
-        s1.College = "MIT";
-
-        System.out.println("s1 Changed The College Name Now Let Us See Is It Changed For s2 Also ?");
-        s1.StudentDetail();
-        s2.StudentDetail();
-        System.out.println("Yes , if Any Object Change That Variable That Changes Will Be Reflacted To All Objects");
-
-    }
-
+    System.out.println(
+      "s1 Changed The College Name Now Let Us See Is It Changed For s2 Also ?"
+    );
+    s1.StudentDetail();
+    s2.StudentDetail();
+    System.out.println(
+      "Yes , if Any Object Change That Variable That Changes Will Be Reflacted To All Objects"
+    );
+  }
 }
