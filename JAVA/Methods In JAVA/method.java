108eea8fd7e52e773c3a65367bcf87adef26b9ec
KRUSHI MONPARA <krushimonpara24@gmail.com>
2024-01-26 17:19:25 +0530

    JAVA AND OOPS

--- JAVA/Methods In JAVA/method.java
+++ JAVA/Methods In JAVA/method.java
@@ -22,59 +22,55 @@ Advantage of Method
 
 public class method {
 
-    // Method (Fucntion) Example
-    static void MyMethod() {
-        System.out.println("This Is A Method");
+  // Method (Fucntion) Example
+  static void MyMethod() {
+    System.out.println("This Is A Method");
+  }
+
+  // Parameters and Arguments in Method(Function) #pass Same Data Type Parameters In Method
+  static void Mymethod1(String FirstName, String LastName) {
+    System.out.println("Your First Name Is " + FirstName);
+    System.out.println("Your Last Name IS " + LastName);
+  }
+
+  // Parameters and Arguments in Method(Function) #pass Diffrent Data Type Parameters In Method
+  static void Mymethod2(String Name, int Age) {
+    System.out.println(
+      "Your Name Is " + Name + "." + "Your Age Is " + Age + "."
+    );
+  }
+
+  // Return Value
+  static int Mymethod3(int x, int y) {
+    return x + y;
+  }
+
+  // Return Value
+  static String Mymethod4(String Car) {
+    return Car;
+  }
+
+  // A Method with If...Else
+  static void CheckAge(int Age) {
+    if (Age < 18) {
+      System.out.println("Access denied - You are not old enough!");
+    } else {
+      System.out.println("Access granted - You are old enough!");
     }
+  }
 
-    // Parameters and Arguments in Method(Function) #pass Same Data Type Parameters
-    // In Method
-    static void Mymethod1(String FirstName, String LastName) {
-        System.out.println("Your First Name Is " + FirstName);
-        System.out.println("Your Last Name IS " + LastName);
-    }
+  public static void main(String[] args) {
+    // Call Method(Function)
+    MyMethod();
 
-    // Parameters and Arguments in Method(Function) #pass Diffrent Data Typr
-    // Parameters In Method
-    static void Mymethod2(String Name, int Age) {
-        System.out.println("Your Name Is " + Name + "." + "Your Age Is " + Age + ".");
-    }
+    Mymethod1("KRUSHI", "MONPARA");
 
-    // Return Value
-    static int Mymethod3(int x, int y) {
-        return x + y;
-    }
+    Mymethod2("KRUSHI", 20);
 
-    // Return Value
-    static String Mymethod4(String Car) {
-        return Car;
-    }
+    System.out.println("Your Number Sum Is " + Mymethod3(5, 3));
 
-    // A Method with If...Else
-    static void CheckAge(int Age) {
+    System.out.println("Your Car Name Is " + Mymethod4("Audi"));
 
-        if (Age < 18) {
-            System.out.println("Access denied - You are not old enough!");
-        } else {
-            System.out.println("Access granted - You are old enough!");
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-        // Call Method(Function)
-        MyMethod();
-
-        Mymethod1("KRUSHI", "MONPARA");
-
-        Mymethod2("KRUSHI", 20);
-
-        System.out.println("Your Number Sum Is " + Mymethod3(5, 3));
-
-        System.out.println("Your Car Name Is " + Mymethod4("Audi"));
-
-        CheckAge(19);
-
-    }
+    CheckAge(19);
+  }
 }
