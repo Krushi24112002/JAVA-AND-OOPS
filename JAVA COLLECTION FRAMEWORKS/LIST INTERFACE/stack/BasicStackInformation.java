957caa42850ad171ec5a09c81fb5318e91ace1c3
KRUSHI MONPARA <krushimonpara24@gmail.com>
2024-01-27 18:22:22 +0530

    JAVA AND OOPS AND COLLECTION FRAMEWORKS

--- JAVA COLLECTION FRAMEWORKS/LIST INTERFACE/stack/BasicStackInformation.java
+++ JAVA COLLECTION FRAMEWORKS/LIST INTERFACE/stack/BasicStackInformation.java
@@ -1,29 +1,21 @@
 // Stack Data Structure
-/*
- * 
- * stack :- The class is based on the basic principle of last-in-first-out.
- * (LIFO)
- * 
- * # Performing various operations on Stack class #
- * 
- * 1. Adding Elements :- In order to add an element to the stack, we can use the
- * push() method.
- * This push() operation place the element at the top of the stack.
- * 
- * 2. Accessing the Element :- To retrieve or fetch the first element of the
- * Stack or the element present at the top of the Stack,
- * we can use peek() method. The element retrieved does not get deleted or
- * removed from the Stack.
- * 
- * 3. Removing Elements :- To pop an element from the stack, we can use the
- * pop() method.
- * The element is popped from the top of the stack and is removed from the same.
- * 
- * 4. empty() :- It returns true if nothing is on the top of the stack. Else,
- * returns false.
- * 
- * 5. search() :- It determines whether an object exists in the stack. If the
- * element is found,It returns the position of the element from the top of the
- * stack. Else, it returns -1.
- * 
+/**
+stack :- The class is based on the basic principle of last-in-first-out.(LIFO)
+
+# Performing various operations on Stack class #
+
+1. Adding Elements :- In order to add an element to the stack, we can use the push() method.
+   This push() operation place the element at the top of the stack.
+
+2. Accessing the Element :- To retrieve or fetch the first element of the Stack or the element present at the top of the Stack,
+   we can use peek() method. The element retrieved does not get deleted or removed from the Stack.
+
+3. Removing Elements :- To pop an element from the stack, we can use the pop() method.
+   The element is popped from the top of the stack and is removed from the same.
+
+4. empty() :- It returns true if nothing is on the top of the stack. Else,returns false.
+
+5. search() :- It determines whether an object exists in the stack. If the element is found,It returns the position of the element from the top of the
+   stack. Else, it returns -1.
+
  */
