e6e61663a6a6a4386748c60d5db287ee5ddc761e
Krushi24112002 <krushimonpara24@gmail.com>
2024-08-27 15:57:38 +0530

    JAVA AND OOPS

--- OOPS IN JAVA/Polymorphism/OOPsPolymorphism.java
+++ OOPS IN JAVA/Polymorphism/OOPsPolymorphism.java
@@ -1,7 +1,7 @@
 // Polymorphism:- The word Polymorphism is a combination of two Greek words, Poly means many and Morph means forms.
 // Definition:- Polymorphism is the Ability of An Object To Take On Many Forms.
 /*
-Two Diffrent Types Of Polymorphism 
-1. Compile Time Polymorphism(Function Overloding)
+Two Different Types Of Polymorphism 
+1. Compile Time Polymorphism(Function Overloading)
 2. Rum Time Polymorphism(Function Overriding)
 */
