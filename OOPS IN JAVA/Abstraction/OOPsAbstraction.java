4d056cac302a571e25e83ba81c40081300d0b36c
Krushi24112002 <krushimonpara24@gmail.com>
2024-08-27 16:19:45 +0530

    JAVA AND OOPS

--- OOPS IN JAVA/Abstraction/OOPsAbstraction.java
+++ OOPS IN JAVA/Abstraction/OOPsAbstraction.java
@@ -1,5 +1,5 @@
 // Abstraction in java(OOPs)
-// Defintion:- Data Abstraction is The Process Of Hiding Certain Details And Showing Only Essential Information To The User.
+// Definition:- Data Abstraction is The Process Of Hiding Certain Details And Showing Only Essential Information To The User.
 
 /*
 # Important Points In Abstract Class
@@ -9,7 +9,7 @@
 3) We can have an abstract class without any abstract method.
 4) There can be a final method in abstract class but any abstract method in class(abstract class) can not be declared as final.
 5) We can define static methods in an abstract class
-6) We can use the abstract keyword for declaring top-level classes (Outerclass) as well as inner classes as abstract
+6) We can use the abstract keyword for declaring top-level classes (outerClass) as well as inner classes as abstract
 7) If a class contains at least one abstract method then compulsory should declare a class as abstract
 8) If the Child class is unable to provide implementation to all abstract methods of the Parent class then we should declare that Child class as
 abstract so that the next level Child class should provide implementation to the remaining abstract method
