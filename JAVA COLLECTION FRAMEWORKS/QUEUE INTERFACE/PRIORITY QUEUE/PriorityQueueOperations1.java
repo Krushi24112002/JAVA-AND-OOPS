37d3adba0acd661a9664b967ecbe2df4843c247e
Krushi24112002 <krushimonpara24@gmail.com>
2024-08-28 14:25:15 +0530

    JAVA AND OOPS

--- JAVA COLLECTION FRAMEWORKS/QUEUE INTERFACE/PRIORITY QUEUE/PriorityQueueOperations1.java
+++ JAVA COLLECTION FRAMEWORKS/QUEUE INTERFACE/PRIORITY QUEUE/PriorityQueueOperations1.java
@@ -31,7 +31,7 @@ public class PriorityQueueOperations1 {
     l1.remove(1);
     System.out.println(l1);
 
-    // valueOf() :- This Method Use To Remove Perticular Value in Queue Using Specific Value.
+    // valueOf() :- This Method Use To Remove Particular Value in Queue Using Specific Value.
     l1.remove(Integer.valueOf(3));
     System.out.println(l1);
 
@@ -44,7 +44,7 @@ public class PriorityQueueOperations1 {
     // equals() :- This method is used to compare the equality of the given element with the elements of the Queue.
     System.out.println(l1.equals(l2));
 
-    // hashCode() :- This method is used to return the hashcode value of the given Queue.
+    // hashCode() :- This method is used to return the hashCode value of the given Queue.
     System.out.println(l1.hashCode());
 
     // isEmpty() :- This method is used to check if the Queue is empty or not. It returns true if the Queue is empty, else false.
